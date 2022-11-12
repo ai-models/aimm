721e9f2d27d3f49f19c9f1195a1f8c1f49ef30c7
Rayr <25773909+Rayrsn@users.noreply.github.com>
2022-11-12 19:45:33 +0200

    Show only latest version, show others at the end

--- cli/search.py
+++ cli/search.py
@@ -29,6 +29,8 @@ def search(name: str, include_adult: bool = typer.Option(False, "--include-adult
         sys.exit(1)
     # print the first 5 results
     count = 0
+    version_list = []
+    typer.echo()
     typer.echo("Results:")
     if include_adult:
         for model in data["data"]:
@@ -36,17 +38,28 @@ def search(name: str, include_adult: bool = typer.Option(False, "--include-adult
                 break
             model_name = model["attributes"]["model_name"]
             versions = model["attributes"]["version"]
+            latest_ver = versions[-1]["version_number"]
+            # only print the last version of every model
             for item in versions:
                 version = item['version_number']
-                if item['adult']:
-                    typer.echo(f"  {model_name}:{version} (Adult content)")
-                else:
-                    typer.echo(f"  {model_name}:{version}")
-                typer.echo(f"    Size: {item['size']}  License: {item['license']}")
-                typer.echo(f"    {main.WEBSITE}/m/{model_name}")
-                typer.echo(f"    {model['attributes']['description']}")
-                typer.echo()
-                count += 1
+                if len(versions) > 1 and version != latest_ver:
+                    version_list.append(item['version_number'])
+            for item in versions:
+                if item['version_number'] == latest_ver:
+                    version = item['version_number']
+                    if item['adult']:
+                        typer.echo(f"  {model_name}:{version} (Adult content)")
+                    else:
+                        typer.echo(f"  {model_name}:{version}")
+                    typer.echo(f"    Size: {item['size']}  License: {item['license']}")
+                    typer.echo(f"    {main.WEBSITE}/m/{model_name}")
+                    typer.echo(f"    {model['attributes']['description']}")
+                    # print other versions in the same line
+                    if len(versions) > 1:
+                        typer.echo("    Other versions:")
+                        typer.echo("      "+", ".join(list(set(version_list))))
+                    typer.echo()
+                    count += 1
                 
     elif only_adult:
         for model in data["data"]:
@@ -54,27 +67,58 @@ def search(name: str, include_adult: bool = typer.Option(False, "--include-adult
                 break
             model_name = model["attributes"]["model_name"]
             versions = model["attributes"]["version"]
+            latest_ver = versions[-1]["version_number"]
+            all_versions_list = []
+            # find the latest adult version
+            # only print the last version of every model
             for item in versions:
                 version = item['version_number']
-                if item['adult']:
-                    typer.echo(f"  {model_name}:{version} (Adult content)")
-                    typer.echo(f"    Size: {item['size']}  License: {item['license']}")
-                    typer.echo(f"    {main.WEBSITE}/m/{model_name}")
-                    typer.echo(f"    {model['attributes']['description']}")
-                    typer.echo()
-                    count += 1
+                all_versions_list.append(version)
+            for version in reversed(all_versions_list):
+                for item in versions:
+                    if item['version_number'] == version:
+                        if item['adult']:
+                            latest_adult_ver = item['version_number']
+                            break
+            for item in versions:
+                if len(versions) > 1 and item['version_number'] != latest_adult_ver:
+                    version_list.append(item['version_number'])
+            for item in versions:
+                if item['version_number'] == latest_adult_ver:
+                    if item['adult']:
+                        typer.echo(f"  {model_name}:{latest_adult_ver} (Adult content)")
+                        typer.echo(f"    Size: {item['size']}  License: {item['license']}")
+                        typer.echo(f"    {main.WEBSITE}/m/{model_name}")
+                        typer.echo(f"    {model['attributes']['description']}")
+                        # print other versions in the same line
+                        if len(versions) > 1:
+                            typer.echo("    Other versions:")
+                            typer.echo("      "+", ".join(list(set(version_list))))
+                        typer.echo()
+                        count += 1
     else:
         for model in data["data"]:
             if count == 5:
                 break
             model_name = model["attributes"]["model_name"]
             versions = model["attributes"]["version"]
+            latest_ver = versions[-1]["version_number"]
+            # only print the last version of every model
             for item in versions:
                 version = item['version_number']
-                if not item['adult']:
-                    typer.echo(f"  {model_name}:{version}")
-                    typer.echo(f"    Size: {item['size']}  License: {item['license']}")
-                    typer.echo(f"    {main.WEBSITE}/m/{model_name}")
-                    typer.echo(f"    {model['attributes']['description']}")
-                    typer.echo()
-                    count += 1
+                if len(versions) > 1 and version != latest_ver:
+                    version_list.append(item['version_number'])
+            for item in versions:
+                if item['version_number'] == latest_ver:
+                    version = item['version_number']
+                    if not item['adult']:
+                        typer.echo(f"  {model_name}:{version}")
+                        typer.echo(f"    Size: {item['size']}  License: {item['license']}")
+                        typer.echo(f"    {main.WEBSITE}/m/{model_name}")
+                        typer.echo(f"    {model['attributes']['description']}")
+                        # print other versions in the same line
+                        if len(versions) > 1:
+                            typer.echo("    Other versions:")
+                            typer.echo("      "+", ".join(list(set(version_list))))
+                        typer.echo()
+                        count += 1
