f9e3a611fed9ac747c7253880bd42b66d690194a
Justin Riddiough <jriddiough@gmail.com>
2022-11-18 23:53:06 -0500

    refactor for nsis installer (works!)

--- aimm.py
+++ aimm.py
@@ -6,8 +6,7 @@ WEBSITE = "https://aimodels.org"
 API_SERVER = "https://api.aimodels.org"
 PROGRAM_NAME = "aimm"
 
-if __name__ == "__main__":
-    # if no arguments are passed or if the first argument is "--help", print help
+def init():    # if no arguments are passed or if the first argument is "--help", print help
     if len(sys.argv) == 1 or sys.argv[1] == "--help":
         aimmApp.show_help(PROGRAM_NAME)
     else:
