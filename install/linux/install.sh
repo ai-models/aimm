7ccf68972a7916b813ff714eed3c9b037a453457
Justin Riddiough <jriddiough@gmail.com>
2022-11-20 07:53:39 -0500

    update to allow direct running, and start of linux installer script

--- install/linux/install.sh
+++ install/linux/install.sh
@@ -3,30 +3,46 @@ echo "| This script will install the AI Model Manager on your   |"
 echo "| system.                                                 |"
 echo "=------------------------------------ [ by VisionInit ] --= "
 
-echo "Install in /usr/local/bin? (y/n)"
-read -r ans
-while [ "$ans" != "y" ] && [ "$ans" != "n" ]; do
-  echo "Invalid response. Please enter 'y' or 'n'."
-  read -r ans
-done
-if [ "$ans" = "y" ]; then
-  install_path=/usr/local/bin
+if [ "$1" = "-y" ]; then
+  DEFAULTS=1
 else
-  echo "Please enter the path to install to:"
-  read -r path
-  # Check if path exists, offer to create it if not
-  if [ ! -d "$path" ]; then
-    echo "Path does not exist. Create it? (y/n)"
-    read -r ans
-    while [ "$ans" != "y" ] && [ "$ans" != "n" ]; do
-      echo "Invalid response. Please enter 'y' or 'n'."
-      read -r ans
-    done
-    if [ "$ans" = "y" ]; then
-      makepath=true
-    else
-      echo "Install cancelled."
+  DEFAULTS=0
+fi
+
+if [ "$DEFAULTS" = "1" ]; then
+  echo "Installing to /usr/local/bin"
+  install_path=/usr/local/bin
+  # check for sudo access
+  if [ "$EUID" -ne 0 ]; then
+    echo "Sudo access required to write to /usr/local/bin"
+    exit 1
+  fi
+else
+  read -p "Install to /usr/local/bin/aimm? (y/n): " -r ans
+  while [ "$ans" != "y" ] && [ "$ans" != "n" ]; do
+    read -p "Invalid response. Please enter 'y' or 'n' : " -r ans
+  done
+  if [ "$ans" = "y" ]; then
+    # check for sudo access
+    if [ "$EUID" -ne 0 ]; then
+      echo "Sudo access required to write to /usr/local/bin"
       exit 1
+    fi
+    install_path=/usr/local/bin
+  else
+    read -p "Please enter the path to install to: " -r path
+    # Check if path exists, offer to create it if not
+    if [ ! -d "$path" ]; then
+      read -p "Path does not exist. Create it? (y/n): " -r ans
+      while [ "$ans" != "y" ] && [ "$ans" != "n" ]; do
+        read -p "Invalid response. Please enter 'y' or 'n' : " -r ans
+      done
+      if [ "$ans" = "y" ]; then
+        makepath=true
+      else
+        echo "Install cancelled."
+        exit 1
+      fi
     fi
   fi
 fi
@@ -43,7 +59,7 @@ fi
 # download the latest version of the program from github binary
 echo "Downloading latest version of AIMM..."
 download_path=$(curl -s https://api.github.com/repos/visioninit/ai-models-cli/actions/artifacts\?per_page\=9 | jq '[.artifacts[] | {name : .name, archive_download_url : .archive_download_url}]' | jq -r '.[] | select (.name == "Linux") | .archive_download_url')
-wget downlowd_path -O $install_path/aimm
+wget download_path -O $install_path/aimm
 
 # make the program executable if file exists
 if [ -f "$install_path/aimm" ]; then
