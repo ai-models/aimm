7061367ad7a8d2f9b0a8808b13fd9d58516ae292
Justin Riddiough <jriddiough@gmail.com>
2022-12-02 03:04:18 -0500

    test [build-win]

--- resources/install/win-installer.bat
+++ resources/install/win-installer.bat
@@ -6,10 +6,10 @@ pynsist.exe "win-build.tmp"
 
 @REM  read installer_name value from installer.cfg
 FOR /F "tokens=2 delims==" %%a IN ('findstr /i "installer_name" "win-build.tmp"') DO SET installer_name=%%a
-mkdir "dist\win-installer"
+mkdir "out\"
 
 @REM copy installer to dist folder
-move "build\nsis\%installer_name%" "dist\win-installer\%installer_name%"
-call install\win\sha256.bat "dist\win-installer\%installer_name%" sha256
-echo %sha256% > "dist\win-installer\%installer_name%.sha256.txt"
+move "build\nsis\%installer_name%" "out\%installer_name%"
+call install\win\sha256.bat "out\%installer_name%" sha256
+echo %sha256% > "out\%installer_name%.sha256.txt"
 DEL  "win-build.tmp"
