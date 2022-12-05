ed6369912d222201b516a79befb497efb9753e47
Justin Riddiough <jriddiough@gmail.com>
2022-12-05 12:15:41 -0500

    refine record script

--- resources/actions/autocon/gif_automate.py
+++ resources/actions/autocon/gif_automate.py
@@ -34,5 +34,5 @@ if __name__ == '__main__':
                     # read the json file
                     json_data = scan_json(f"{DEFAULT_DIR}/src/{name}.json", "config")
 
-                    os.system(f'./agg --rows='+json_data['lines']+' {DEFAULT_DIR}/src/{name}.asc {DEFAULT_DIR}/dist/{name}.gif')
+                    os.system(f'./agg --rows='+str(json_data['lines'])+' {DEFAULT_DIR}/src/{name}.asc {DEFAULT_DIR}/dist/{name}.gif')
             
