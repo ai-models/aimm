7e18df21550197ccb0ce702c3cd1377d3fc5e7b6
Justin Riddiough <jriddiough@gmail.com>
2022-12-05 17:14:55 -0500

    update messages [build-linux]

--- aimm.py
+++ aimm.py
@@ -11,7 +11,7 @@ VERSION = "alpha-4"
 def init():
     for arg in sys.argv:
         if arg in ('--version', '-v'):
-            print(f"AIMM (AI Model Manager) Version: {VERSION}")
+            print(f"aimm AI Model Manager Version: {VERSION}")
             sys.exit(0)
         if arg in ('--licenses'):
             if '--verbose' in sys.argv:
