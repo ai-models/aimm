0b8f47f20aae5a8b122df300a62772bbcee4ebfa
Rayrsn <25773909+Rayrsn@users.noreply.github.com>
2022-12-06 19:40:57 +0200

    Fixes argument handling bug

--- aimm.py
+++ aimm.py
@@ -10,17 +10,17 @@ VERSION = "0.4.0"
 
 def init():
     for arg in sys.argv:
-        if arg in ('--version', '-v'):
+        if ('--version', '-v') in arg:
             print(f"AIMM Version: {VERSION}")
             sys.exit(0)
-        if arg in ('--licenses'):
+        if ('--licenses') in arg:
             if '--verbose' in sys.argv:
                 aimmApp.show_licenses(verbose=True)
                 sys.exit(0)
             else:
                 aimmApp.show_licenses()
                 sys.exit(0)
-        if arg in ('--check-update'):
+        if ('--check-update') in arg:
             aimmApp.check_for_updates(GITHUB_REPO, VERSION)
             sys.exit(0)
     if len(sys.argv) == 1 or sys.argv[1] == "--help": # if no arguments are passed or if the first argument is "--help", print help
