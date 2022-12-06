b6dad265e6ca5111f784ee7dc6c5fba973035f58
Justin Riddiough <jriddiough@gmail.com>
2022-12-06 01:10:30 -0500

    try to resolve circular import [build-linux]

--- cli/scan.py
+++ cli/scan.py
@@ -3,9 +3,7 @@ import hashlib
 import json
 import typer
 from picklescan import scanner
-
-from cli import aimmApp
-from cli.base_funcs import extract_name_version, get_last_version
+from cli import aimmApp, base_funcs
 
 app = aimmApp.app
 
@@ -19,16 +17,16 @@ def hash_file(filename):
 
     return hash_obj.hexdigest()
 
-def get_model_path(name_version):
-    name, version = extract_name_version(name_version)
-    if version is None:
-        version = get_last_version(name)
-    for package in aimmApp.installed["packages"]:
-        if package["name"].lower() == name.lower() and package["version"] == version:
-            return package["paths"]
-    else:
-        typer.echo(f"Error: {name}:{version} not found")
-        return None
+# def get_model_path(name_version):
+#     name, version = extract_name_version(name_version)
+#     if version is None:
+#         version = get_last_version(name)
+#     for package in aimmApp.installed["packages"]:
+#         if package["name"].lower() == name.lower() and package["version"] == version:
+#             return package["paths"]
+#     else:
+#         typer.echo(f"Error: {name}:{version} not found")
+#         return None
 
 def get_maximum_danger(result_globals) -> str:
     safety = None
@@ -51,29 +49,29 @@ def scan(name_version: str, raw: bool = typer.Option(False, "--raw", "-r")):
     """
     Scan a file for malicious code.
     """
-    model_path = get_model_path(name_version)
-    if model_path:
-        for file in os.listdir(model_path):
-            path = os.path.join(model_path, file)
-            result = scanner.scan_file_path(path)
-            if not raw:
-                print(f"Scanning {path}...")
-                print(f"scanned files: {result.scanned_files}")
-                print(f"issues count: {result.issues_count}")
-                print(f"infected files: {result.infected_files}")
-                print("safety level of modules:")
-                for module in result.globals:
-                    print(
-                        f"  * {module.module}.{module.name} - {(module.safety.value).title()}")
-                print(f"Hash: {hash_file(path)}")
-            else:
-                safety = get_maximum_danger(result.globals)
-                print(json.dumps({
-                    "hash": hash_file(path),
-                    "picklescan": {
-                        "scanned_files": result.scanned_files,
-                        "issues_count": result.issues_count,
-                        "infected_files": result.infected_files,
-                        "safety_level": safety
-                    }
-                },indent=4))
+    # model_path = get_model_path(name_version)
+    # if model_path:
+    #     for file in os.listdir(model_path):
+    #         path = os.path.join(model_path, file)
+    #         result = scanner.scan_file_path(path)
+    #         if not raw:
+    #             print(f"Scanning {path}...")
+    #             print(f"scanned files: {result.scanned_files}")
+    #             print(f"issues count: {result.issues_count}")
+    #             print(f"infected files: {result.infected_files}")
+    #             print("safety level of modules:")
+    #             for module in result.globals:
+    #                 print(
+    #                     f"  * {module.module}.{module.name} - {(module.safety.value).title()}")
+    #             print(f"Hash: {hash_file(path)}")
+    #         else:
+    #             safety = get_maximum_danger(result.globals)
+    #             print(json.dumps({
+    #                 "hash": hash_file(path),
+    #                 "picklescan": {
+    #                     "scanned_files": result.scanned_files,
+    #                     "issues_count": result.issues_count,
+    #                     "infected_files": result.infected_files,
+    #                     "safety_level": safety
+    #                 }
+    #             },indent=4))
