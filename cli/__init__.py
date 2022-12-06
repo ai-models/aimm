89857c844197ed24fa444354a4e5e7a3a5aa4c7c
Justin Riddiough <jriddiough@gmail.com>
2022-12-06 01:15:56 -0500

    try to resolve circular import [build-linux]

--- cli/__init__.py
+++ cli/__init__.py
@@ -3,12 +3,12 @@ from cli import (base_funcs,
                  install,
                  uninstall,
                  info,
+                 scan,
                  search,
                  init,
                  list,
                  add,
                  remove,
                  security,
-                 credentials,
-                 scan
+                 credentials
                  )
