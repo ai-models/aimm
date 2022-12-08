7067057acd90f5d37557a87467b40c2b736683af
Justin Riddiough <jriddiough@gmail.com>
2022-12-08 11:10:43 -0500

    try again with setup commands

--- resources/actions/autocon/json_typing.py
+++ resources/actions/autocon/json_typing.py
@@ -20,7 +20,7 @@ def scan_json(path_to_file,key=None):
         return json_data[key]
 
 def print_data(json_data, setup=None):
-    if os.path.exists("/home.runner/local/share/aimm"):
+    if os.path.exists("/home/runner/local/share/aimm"):
         os.system("rm -rf /home.runner/local/share/aimm")
     if os.path.exists("aimodels.json"):
         os.system("rm aimodels.json")
