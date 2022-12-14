bc157dc0b0305f869e5a0282cb65a107d2ece762
Rayrsn <25773909+Rayrsn@users.noreply.github.com>
2022-12-14 20:53:00 +0200

    fix bug where version was executed

--- aimm.py
+++ aimm.py
@@ -10,17 +10,17 @@ VERSION = "0.5.0"
 
 def init():
     for arg in sys.argv:
-        if ('--version') in arg or ('-v') in arg:
+        if (' --version') in arg or (' -v') in arg:
             print(f"AIMM Version: {VERSION}")
             sys.exit(0)
-        if ('--licenses') in arg:
+        elif (' --licenses') in arg:
             if '--verbose' in sys.argv:
                 aimmApp.show_licenses(verbose=True)
                 sys.exit(0)
             else:
                 aimmApp.show_licenses()
                 sys.exit(0)
-        if ('--check-update') in arg:
+        elif (' --check-update') in arg:
             aimmApp.check_for_updates(GITHUB_REPO, VERSION)
             sys.exit(0)
     if len(sys.argv) == 1 or sys.argv[1] == "--help": # if no arguments are passed or if the first argument is "--help", print help
